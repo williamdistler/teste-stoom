c6105f65926c80a138b6f19054ee7a6ad92a4dcb
williamdistler <william.distler@icloud.com>
2024-08-01 15:02:41 -0300

    FIX: Erro de nomenclatura classe de teste

--- src/test/java/com/william/teste_stoom/ProdutoControllerTests.java
+++ src/test/java/com/william/teste_stoom/ProdutoControllerTests.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ProdutoControllerTest {
+class ProdutoControllerTests {
 
     @Mock
     private ProdutoService produtoService;
